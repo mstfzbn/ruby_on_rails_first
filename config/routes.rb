2ca76b3d7dc0b32c6efddd8a1b79948dde337bca
mstfzbn <midnight90@mail.bg>
2021-11-09 00:14:14 +0200

    Experimental views for models and brands.

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   get 'car_model/show_car_brand'
 
 
-     root 'car_brand#list'
+     root 'car_model#list'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
